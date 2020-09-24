179554872e483fb64516d69d94feb5c0e787f005
Zubisofts <zubitex40@gmail.com>
2020-09-24 19:43:58 +0100

    Added infoIntent

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent infoIntent  =new Intent(this,InfoActivity.class);
-        startActivity(infoIntent );
+        startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
 }
