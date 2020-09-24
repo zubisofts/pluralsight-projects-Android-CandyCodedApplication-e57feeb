3fcbf6dd5cb8c6d1b4338a14a6ccd6a388b55ed9
Zubisofts <zubitex40@gmail.com>
2020-09-24 20:16:01 +0100

    Added phoneIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -40,4 +40,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent phoneIntent=new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 }
